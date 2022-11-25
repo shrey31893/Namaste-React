a20c50d9f15f9ccf1065a756545bc29f4ac74328
Shrey Shah <SShah@bankersfinancialcorp.com>
2022-11-25 15:03:39 +0530

    added cra app and normal building app.

---
+++ Day-2-Ignite/src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement("h1", {}, "hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
