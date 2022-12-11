2499a183b663720a51f7a27214c66979696c1f47
Shrey Shah <SShah@bankersfinancialcorp.com>
2022-12-11 18:37:30 +0530

    day-7 changes

--- Day-7-FindingThePath/src/App.js
+++ Day-7-FindingThePath/src/App.js
@@ -8,6 +8,15 @@ import { title } from "./utils/constants.js"; // -- named export.
 // import * as constants from './constants.js'  -- usage -- {{constants.title}}
 import SearchBar from "./components/SearchBar.js";
 import NoResultFoundComponent from "./components/NoResultFoundComponent.js";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import AboutUs from "./components/AboutUs.js";
+import ErrorComponent from "./components/ErrorComponent.js";
+import RestaurantComponent from "./components/RestaurantComponent.js";
 
 const HeadingComponent = () => {
   return (
@@ -20,12 +29,16 @@ const HeadingComponent = () => {
 const CardContainer = ({ filteredRestaurants }) => {
   if (filteredRestaurants.length == 0) return <NoResultFoundComponent />;
 
-  const cards = filteredRestaurants && filteredRestaurants.map((restaurantData, index) => {
-    return (
-      <CardComponent restaurant={restaurantData} key={restaurantData.id} />
-    );
-    // return <CardComponent restaurant= {data[index]}/> OR Way using index.
-  });
+  const cards =
+    filteredRestaurants &&
+    filteredRestaurants.map((restaurantData, index) => {
+      return (
+        <Link to={`/restaurant/${restaurantData?.data?.id}`}>
+          <CardComponent restaurant={restaurantData} key={restaurantData?.data?.id} />
+        </Link>
+      );
+      // return <CardComponent restaurant= {data[index]}/> OR Way using index.
+    });
   return cards;
 };
 
@@ -33,7 +46,7 @@ const CardContainer1 = () =>
   data.map((data, index) => <CardComponent restaurant={data} />);
 
 const BodyComponent = () => {
-   //const [filteredRestaurants, setFilteredRestaurants] = useState(data);
+  //const [filteredRestaurants, setFilteredRestaurants] = useState(data);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [restaurants, setRestaurants] = useState([]);
 
@@ -43,13 +56,18 @@ const BodyComponent = () => {
 
   return (
     <div className="Card-Container">
-      <SearchBar setFilteredRestaurants={setFilteredRestaurants} restaurants = {restaurants}/>
+      <SearchBar
+        setFilteredRestaurants={setFilteredRestaurants}
+        restaurants={restaurants}
+      />
       <CardContainer filteredRestaurants={filteredRestaurants} />
     </div>
   );
 
   async function fetchData() {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.196537&lng=72.63304&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.196537&lng=72.63304&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
     console.log(json);
     console.log(json?.data?.cards[2]?.data?.data?.cards);
@@ -62,12 +80,35 @@ const AppLayoutComponent = () => {
   return (
     <>
       <HeadingComponent />
-      <BodyComponent />
+      <Outlet />
+      {/* THIS IS COMMENTED FOR CHILD ROUTES - <BodyComponent /> */}
       {/* <hr/> */}
-      <BeautifulCardComponent />
+      {/* <BeautifulCardComponent /> */}
     </>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayoutComponent />, //JSX not without JSX.
+    errorElement: <ErrorComponent />,
+    children: [
+      {
+        path: "/restaurant/:id",
+        element: <RestaurantComponent />,
+      },
+      {
+        path: "/search",
+        element: <BodyComponent />,
+      },
+    ],
+  },
+  {
+    path: "/aboutus",
+    element: <AboutUs />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayoutComponent />);
+root.render(<RouterProvider router={appRouter} />);
